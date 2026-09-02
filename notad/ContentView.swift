8f47b9448aac80971fe85bdad23db59decfc3e74
artomily <vararakya@gmail.com>
2026-09-02 21:47:56 +0700

    Initial Commit

---
+++ notad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  notad
+//
+//  Created by yourbaemac on 02/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ notad/notadApp.swift
@@ -0,0 +1,17 @@
+//
+//  notadApp.swift
+//  notad
+//
+//  Created by yourbaemac on 02/09/26.
+//
+
+import SwiftUI
+
+@main
+struct notadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
